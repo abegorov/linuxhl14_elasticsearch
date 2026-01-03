6a89f4491345efbcc21e8f60edd78c0ad5a40be7
abegorov <abegorov@github.com>
2026-01-03 17:58:31 +0300

    terraform: lb listen kibana and vts

--- main.tf
+++ main.tf
@@ -95,6 +95,20 @@ resource "yandex_lb_network_load_balancer" "backend" {
       ip_version = "ipv4"
     }
   }
+  listener {
+    name = "${var.project}-backend-kibana"
+    port = 5601
+    external_address_spec {
+      ip_version = "ipv4"
+    }
+  }
+  listener {
+    name = "${var.project}-backend-vts"
+    port = 9443
+    external_address_spec {
+      ip_version = "ipv4"
+    }
+  }
   attached_target_group {
     target_group_id = yandex_lb_target_group.backend.id
     healthcheck {
